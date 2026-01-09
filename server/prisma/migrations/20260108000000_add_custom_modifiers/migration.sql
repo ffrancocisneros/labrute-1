78eb5b1b308dc6ca6aec85dfcd846afe56d12179
Franco <franco@example.com>
2026-01-09 15:53:53 -0300

    Add Prisma migration for custom modifiers enum values

---
+++ server/prisma/migrations/20260108000000_add_custom_modifiers/migration.sql
@@ -0,0 +1,7 @@
+-- AlterEnum
+-- Add new values to FightModifier enum
+ALTER TYPE "FightModifier" ADD VALUE 'doubleXP';
+ALTER TYPE "FightModifier" ADD VALUE 'doubleHP';
+ALTER TYPE "FightModifier" ADD VALUE 'noPets';
+ALTER TYPE "FightModifier" ADD VALUE 'allHaveBear';
+ALTER TYPE "FightModifier" ADD VALUE 'doubleFights';
